d5f57565bfa366e010b7ec41e0c20091cd7d8811
NoOneTopVN <huynhtrongnghia15n@gmail.com>
2024-08-29 11:03:34 +0700

    54. Test Catalog API - CreateProduct POST Request - Debug Carter Minimal API and Med

---
+++ src/Services/Catalog/Catalog API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog_API.Models;
+using MediatR;
+
+namespace Catalog_API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+: ICommand<CreateProductResponse>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
+{
+    public async Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //create Product entity from command object
+        //save to database
+        //return CreateProductResult result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //TODO
+        //save to database
+        //return result
+
+        return new CreateProductResponse(Guid.NewGuid());
+    }
+}
