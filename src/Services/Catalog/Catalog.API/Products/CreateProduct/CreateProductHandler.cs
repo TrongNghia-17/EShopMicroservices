54d24877851c465643c1f921b8ea88d16f0db932
NoOneTopVN <huynhtrongnghia15n@gmail.com>
2024-11-19 14:26:53 +0700

    Implement CQRS Abstractions ICommand into Vertical Slice Feature Handler class

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,33 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
-public class CreateProductHandler
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //create Product entity from command object
+        //save to database
+        //return CreateProductResult result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // TODO
+        //save to database
+        //return result
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
 }
