13a8e908b518a1b2456ac2f3cc3ae63719966b56
NoOneTopVN <huynhtrongnghia15n@gmail.com>
2024-11-25 19:31:52 +0700

    Validation Pipeline Behaviour with MediatR using Fluent Validation Library

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -2,6 +2,7 @@
 namespace Catalog.API.Products.DeleteProduct;
 
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
 public record DeleteProductResult(bool IsSuccess);
 
 internal class DeleteProductCommandHandler
